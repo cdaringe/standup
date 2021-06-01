6dcc32ce02bc7d8f5cdcb428304c85dd8a69493c
cdaringe <cdaringe@users.noreply.github.com>
2021-06-01 06:56:24 +0000

    Deploying to gh-pages from @ cdaringe/standup@aafb0d48c516738d5107d885baf9d2133aff455e 🚀

---
+++ _next/static/f8gypE3KAtKLQ_6Vf6QMV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3e173ed540cb9398e1a2.js"],"/_error":["static/chunks/pages/_error-1745c911a1ad58fecb7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
